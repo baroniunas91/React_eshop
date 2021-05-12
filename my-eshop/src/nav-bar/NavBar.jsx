508c0928af73d2b658777fa19b2c89c484aaadea
Edgaras Baroniūnas <ebaroniunas@alnasoft.lt>
2021-05-12 19:04:36 +0300

    Implement NavBar links

--- my-eshop/src/nav-bar/NavBar.jsx
+++ my-eshop/src/nav-bar/NavBar.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 function NavBar () {
     return (
         <nav className="bg-blue-400">
             <ul className="flex">
-                <li className="p-2 text-xl font-semibold">PRODUCTS</li>
-                <li className="p-2 text-xl font-semibold">VISION</li>
+                <li className="p-2 text-xl font-semibold hover:text-white">
+                    <Link to ="/">PRODUCTS</Link>
+                </li>
+                <li className="p-2 text-xl font-semibold hover:text-white">
+                    <Link to ="/vision">VISION</Link>
+                </li>
             </ul>
         </nav>
     )
