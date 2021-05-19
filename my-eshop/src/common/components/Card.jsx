e026ad0f4f08b4952dbc33f51dd769a570aae689
Edgaras Baroniūnas <ebaroniunas@alnasoft.lt>
2021-05-19 19:45:31 +0300

    Add reusable card component | Use card component and products

---
+++ my-eshop/src/common/components/Card.jsx
@@ -0,0 +1,13 @@
+//bg-white border shadow ${isRound && 'rounded'} p-2 ${className}`
+
+import clsx from "clsx"
+
+function Card({ className, isRound, children }) {
+    return (
+        <div className={clsx('bg-white', 'border', 'shadow', 'p-2', isRound && 'rounded', className)}>
+            {children}
+        </div>
+    )
+}
+
+export default Card
--- my-eshop/src/products/Products.jsx
+++ my-eshop/src/products/Products.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Card from "../common/components/Card";
 
 function Products() {
     const [products, setProducts] = useState([]);
@@ -18,7 +19,7 @@ function Products() {
         <ul className="flex flex-wrap">
             {products.map((x) => (
                 <li className="p-2" key={x.id}>
-                    <div className="w-64 h-80 bg-white border shadow rounded p-2">
+                    <Card className="w-64 h-80" isRound>
                         <div className="h-3/4 p-4">
                             <img className="max-h-full m-auto" src={x.image} alt={x.title} />
                         </div>
@@ -26,7 +27,7 @@ function Products() {
                         <div className="text-center font-bold truncate" title={x.title}>{x.title}</div>
 
                         <div className="text-center">{x.price}$</div>
-                    </div>
+                    </Card>
                 </li>
             ))}
         </ul>
