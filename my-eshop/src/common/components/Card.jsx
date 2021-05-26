ed4cdc54243b382ba7b8002c3238f88561bd013c
Edgaras Baroniūnas <ebaroniunas@alnasoft.lt>
2021-05-26 18:57:24 +0300

    Update Card.jsx

--- my-eshop/src/common/components/Card.jsx
+++ my-eshop/src/common/components/Card.jsx
@@ -1,12 +1,10 @@
-//bg-white border shadow ${isRound && 'rounded'} p-2 ${className}`
-
 import clsx from "clsx"
 
-// const defaultClasses = 'bg-white border shadow p-2 rounded flex flex-col';
+const defaultClasses = 'bg-white border shadow p-2 flex flex-col';
 
 function Card({ className, isRound, children }) {
     return (
-        <div className={clsx('bg-white', 'border', 'shadow', 'p-2', isRound && 'rounded', className, 'flex', 'flex-col')}>
+        <div className={clsx(defaultClasses, isRound && 'rounded', className)}>
             {children}
         </div>
     )
