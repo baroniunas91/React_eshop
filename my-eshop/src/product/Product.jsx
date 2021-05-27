0837bff45f0b62be076026b2e5a8c6f2b039f929
Edgaras Baroniūnas <ebaroniunas@alnasoft.lt>
2021-05-27 10:05:28 +0300

    Put react libraries to top

--- my-eshop/src/product/Product.jsx
+++ my-eshop/src/product/Product.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Card from '../common/components/Card';
 import { getProduct } from '../common/requests';
 import Counter from '../common/components/Counter';
