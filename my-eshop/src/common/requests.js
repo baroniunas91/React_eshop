a837b7644d7a3038f77a230efb244b86e6784b22
Edgaras Baroniūnas <ebaroniunas@alnasoft.lt>
2021-05-27 10:04:36 +0300

    update request.js

--- my-eshop/src/common/requests.js
+++ my-eshop/src/common/requests.js
@@ -1,15 +1,11 @@
-const fakeStoreApiUtl = "https://fakestoreapi.com";
+const fakeStoreApiUtl = 'https://fakestoreapi.com';
 
 export const getProducts = {
   url: `${fakeStoreApiUtl}/products`,
-  method: "GET",
+  method: 'GET',
 };
 
 export const getProduct = (id) => ({
   url: `${fakeStoreApiUtl}/products/${id}`,
-  method: "GET",
+  method: 'GET',
 });
-
-// export function getProductES5(id) {
-//     return { url: `https://fakestoreapi.com/products/${id}`, method: 'GET'}
-// }
