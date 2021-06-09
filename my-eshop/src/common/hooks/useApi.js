72c3985694cb9022db7af35057b9c950d9bb7eb6
Edgaras Baroniūnas <baroniunas@gmail.com>
2021-06-09 19:22:22 +0300

    Add try catch to useApi

--- my-eshop/src/common/hooks/useApi.js
+++ my-eshop/src/common/hooks/useApi.js
@@ -10,9 +10,14 @@ function useApi() {
 
   async function call(request) {
     setState((prev) => ({ ...prev, isLoading: true }));
-    const { data } = await axios(request);
-
-    setState({ data, isLoading: false });
+    try {
+      const { data } = await axios(request);
+      setState({ data, isLoading: false });
+    } catch (e) {
+      setState((prev) => ({ ...prev, isLoading: false }));
+      console.error(e);
+      //redirect to error page
+    }
   }
 
   return {
