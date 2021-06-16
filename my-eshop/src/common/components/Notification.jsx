9603300093b786b8d5c040c78443276b879581af
Edgaras Baroniūnas <baroniunas@gmail.com>
2021-06-16 19:53:02 +0300

    Add slide-out animation

--- my-eshop/src/common/components/Notification.jsx
+++ my-eshop/src/common/components/Notification.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { render, unmountComponentAtNode } from 'react-dom';
 
+const unmountDelay = 200;
 const notificationContainer = document.getElementById('notification');
 export const notificationTypes = {
   SUCCESS: 'success',
@@ -12,15 +13,42 @@ const typeClassMap = {
   info: 'bg-blue-500',
 };
 const initialClasses =
-  'fixed text-white font-semibold bottom-3 right-3 p-3 z-50 shadow animate-slide-in';
+  'fixed text-white font-semibold bottom-3 right-3 p-3 z-50 shadow cursor-pointer';
 
 function Notification({ text, type, hangTimeInMS = 3000 }) {
+  const [timeoutId, setTimeoutId] = useState();
+  const [isUnmounting, setIsUnmounting] = useState(false);
+
   useEffect(() => {
+    const timeoutId = setTimeout(unmount, hangTimeInMS);
+    setTimeoutId(timeoutId);
+  }, []); //eslint-disable-line
+
+  function unmount() {
+    setIsUnmounting(true);
+
     setTimeout(() => {
       unmountComponentAtNode(notificationContainer);
-    }, hangTimeInMS);
-  }, []); //eslint-disable-line
-  return <div className={clsx(initialClasses, typeClassMap[type])}>{text}</div>;
+    }, unmountDelay);
+  }
+
+  function handleClick() {
+    clearTimeout(timeoutId);
+    unmount();
+  }
+
+  return (
+    <div
+      className={clsx(
+        initialClasses,
+        typeClassMap[type],
+        isUnmounting ? 'animate-slide-out' : 'animate-slide-in'
+      )}
+      onClick={handleClick}
+    >
+      {text}
+    </div>
+  );
 }
 
 export const notification = {
--- my-eshop/tailwind.config.js
+++ my-eshop/tailwind.config.js
@@ -12,9 +12,18 @@ module.exports = {
             transform: 'translateX(0%)',
           },
         },
+        'slide-out': {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
       },
       animation: {
         'slide-in': 'slide-in 0.3s',
+        'slide-out': 'slide-out 0.3s',
       },
     },
   },
