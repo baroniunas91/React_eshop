ef4fac5fa5cb05aa9834173c6ba112685129c0fa
Edgaras Baroniūnas <baroniunas@gmail.com>
2021-06-21 19:22:51 +0300

    use CartContext in CartNavItem to show no of products in cart

--- my-eshop/src/App.jsx
+++ my-eshop/src/App.jsx
@@ -5,22 +5,25 @@ import NavBar from './nav-bar/NavBar';
 import Product from './product/Product';
 import Products from './products/Products';
 import Vision from './vision/Vision';
+import CartProvider from './common/providers/CartProvider';
 
 function App() {
   return (
     <Router>
-      <header>
-        <NavBar />
-      </header>
-      <main className="p-2" style={{ height: 'calc(100vh - 48px)' }}>
-        <Switch>
-          <Route exact path={['/', '/products']} component={Products} />
-          <Route exact path="/products/:id" component={Product} />
-          <Route exact path="/vision" component={Vision} />
-          <Route exact path="/contacts" component={Contacts} />
-          <Route exact path="/cart" component={Cart} />
-        </Switch>
-      </main>
+      <CartProvider>
+        <header>
+          <NavBar />
+        </header>
+        <main className="p-2" style={{ height: 'calc(100vh - 48px)' }}>
+          <Switch>
+            <Route exact path={['/', '/products']} component={Products} />
+            <Route exact path="/products/:id" component={Product} />
+            <Route exact path="/vision" component={Vision} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route exact path="/cart" component={Cart} />
+          </Switch>
+        </main>
+      </CartProvider>
     </Router>
   );
 }
--- my-eshop/src/nav-bar/components/CartNavItem.jsx
+++ my-eshop/src/nav-bar/components/CartNavItem.jsx
@@ -1,12 +1,32 @@
+import { useContext } from 'react';
 import NavItem from './NavItem';
 import { ReactComponent as CartIconSvg } from '../../common/assets/shopping-cart.svg';
+import { CartContext } from '../../common/providers/CartProvider';
+
+function CartIconWithNo({ productCount }) {
+  return (
+    <>
+      <CartIconSvg className="w-8 h-8" />
+      {productCount && (
+        <span
+          className="absolute text-yellow-400 font-bold text-xs top-1/4"
+          style={{ right: '35%' }}
+        >
+          {productCount}
+        </span>
+      )}
+    </>
+  );
+}
 
 function CartNavItem() {
+  const { cart } = useContext(CartContext);
+
   return (
     <NavItem
-      content={<CartIconSvg className="w-8 h-8" />}
+      content={<CartIconWithNo productCount={cart?.products?.length} />}
       route="/cart"
-      className="ml-auto mr-2"
+      className="ml-auto mr-2 relative"
     />
   );
 }
